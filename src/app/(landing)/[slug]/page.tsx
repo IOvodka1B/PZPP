f817233d5dc4417b8b52cc9a2a0854fd96523500
Igor <i.kadziela03@gmail.com>
2026-04-14 17:02:31 +0200

    feat(landing): dodano runtime do obsługi checkouta na opublikowanych landingach
    Podpięto handler kliknięcia dla przycisku kup-teraz w HTML z GrapesJS.

--- src/app/(landing)/[slug]/page.tsx
+++ src/app/(landing)/[slug]/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 import { getLandingPageBySlug } from "@/app/actions/landingPageActions";
+import LandingRuntime from "./LandingRuntime";
 
 export const dynamic = "force-dynamic";
 
@@ -28,24 +29,6 @@ export default async function LandingRootSlugPage({ params }: PageProps) {
   const htmlData = page.htmlData || "";
   const cssData = page.cssData || "";
 
-  return (
-    <div style={{ width: "100vw", minHeight: "100vh", margin: 0, padding: 0 }}>
-      <style
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{
-          __html: `
-            html, body { margin: 0; padding: 0; height: 100%; width: 100%; }
-            body { overflow-x: hidden; }
-            ${cssData}
-          `,
-        }}
-      />
-      <main
-        style={{ width: "100%", minHeight: "100vh" }}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: htmlData }}
-      />
-    </div>
-  );
+  return <LandingRuntime htmlData={htmlData} cssData={cssData} />;
 }
 
